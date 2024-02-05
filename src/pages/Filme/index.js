51b39caedbae86035f6af58d89c26494d37cdb4c
Isabella Mariano <isabellamariano@outlook.com>
2024-02-05 16:05:31 -0400

    prime flix final

---
+++ src/pages/Filme/index.js
@@ -0,0 +1,91 @@
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { toast } from "react-toastify";
+import api from "../../services/api";
+import "./filme.css";
+
+function Filme() {
+  const { id } = useParams();
+  const [filme, setFilme] = useState({});
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    async function loadFilme() {
+      await api
+        .get(`/movie/${id}`, {
+          params: {
+            api_key: "2d42c9208914f51fc7387e9971ac476a",
+            language: "pt-BR",
+          },
+        })
+        .then((r) => {
+          setFilme(r.data);
+          setLoading(false);
+        })
+        .catch(() => {
+          navigate("*", { replace: true });
+          return;
+        });
+    }
+
+    loadFilme();
+    return () => {
+      console.log("desmontado");
+    };
+  }, [navigate, id]);
+
+  function salvarFilme() {
+    const minhaLista = localStorage.getItem("@primeflix");
+    let filmesSalvos = JSON.parse(minhaLista) || [];
+
+    const hasFilme = filmesSalvos.some(
+      (filmesSalvos) => filmesSalvos.id === filme.id
+    );
+    if (hasFilme) {
+      toast.warning("Filme já está na lista");
+      return;
+    }
+
+    filmesSalvos.push(filme);
+    localStorage.setItem("@primeflix", JSON.stringify(filmesSalvos));
+    toast.success("Salvo com sucesso");
+  }
+
+  if (loading) {
+    return (
+      <div className="loading">
+        <h2>Carregando detalhes...</h2>
+      </div>
+    );
+  }
+
+  return (
+    <div className="filme-info">
+      <h1>{filme.title}</h1>
+      <img
+        src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`}
+        alt={filme.title}
+      />
+      <h3>Sinopse</h3>
+      <span>{filme.overview}</span>
+      <strong>Avaliação: {filme.vote_average} / 10</strong>
+
+      <div className="area-buttons">
+        <button onClick={salvarFilme}>Salvar</button>
+        <button>
+          <a
+            href={`https://youtube.com/results?search_query=${filme.title} Trailer`}
+            alt={`Busca trailer filme ${filme.title}`}
+            target="blank"
+            rel="external"
+          >
+            Trailer
+          </a>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Filme;
---
+++ src/services/api.js
@@ -0,0 +1,10 @@
+//Host
+//https://api.themoviedb.org/3/
+
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://api.themoviedb.org/3/",
+});
+
+export default api;
